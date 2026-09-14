72ce2c8df779c85cac5245573cdb5d45b8622d21
Md. Shafayat Hossain Sadid <mdshafayathossainsadid@gmail.com>
2026-09-14 12:37:23 +0300

    better auth setup done

---
+++ src/lib/auth.js
@@ -0,0 +1,42 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins"
+
+const client = new MongoClient(`${process.env.MONGODB_URI}`);
+const db = client.db("monohor");
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, { client }),
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        },
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                defaultValue: null,
+                input: false,
+            },
+        },
+    },
+    session: {
+        cookieCache: {
+            enabled: true,
+            strategy: "jwt",
+            maxAge: 7 * 24 * 60 * 60
+        }
+    },
+    plugins: [
+        jwt(),
+    ]
+
+});
+
+export { db };
